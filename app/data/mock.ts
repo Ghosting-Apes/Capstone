c7adb80aedf51dddff340a506b4e8b73c3727ba6
nbybhen <henockdinberu11@gmail.com>
2025-05-02 02:38:03 +0000

    feat: button for sorting usercards

--- app/data/mock.ts
+++ app/data/mock.ts
@@ -2,13 +2,19 @@ interface Metadata {
     age: number,
     birthday: string,
     origin: string,
+    yearRange: TimePeriod
 }
   
-interface Person {
+export interface Person {
   name: string,
   data: Metadata,
   desc: string,
   url: string,
+}
+
+export interface TimePeriod {
+  start: number,
+  end: number
 }
   
 export const people: Person[] = [
@@ -18,6 +24,7 @@ export const people: Person[] = [
     data: {
       age: 100,
       birthday: "November 4th, 2002",
+      yearRange: {start: 100, end: 2025},
       origin: "Rome, Italy"
     },
     url: "https://media.istockphoto.com/id/539115110/photo/colosseum-in-rome-and-morning-sun-italy.jpg?s=612x612&w=0&k=20&c=9NtFxHI3P2IBWRY9t0NrfPZPR4iusHmVLbXg2Cjv9Fs="
@@ -28,6 +35,7 @@ export const people: Person[] = [
     data: {
       age: 0,
       birthday: "",
+      yearRange: {start: 2003, end: 2025},
       origin: ""
     },
     url: "https://media.istockphoto.com/id/539115110/photo/colosseum-in-rome-and-morning-sun-italy.jpg?s=612x612&w=0&k=20&c=9NtFxHI3P2IBWRY9t0NrfPZPR4iusHmVLbXg2Cjv9Fs="
@@ -38,6 +46,7 @@ export const people: Person[] = [
     data: {
       age: 0,
       birthday: "",
+      yearRange: {start: 2006, end: 2025},
       origin: ""
     },
     url: "https://media.istockphoto.com/id/539115110/photo/colosseum-in-rome-and-morning-sun-italy.jpg?s=612x612&w=0&k=20&c=9NtFxHI3P2IBWRY9t0NrfPZPR4iusHmVLbXg2Cjv9Fs="
--- app/people/page.tsx
+++ app/people/page.tsx
@@ -1,21 +1,49 @@
 "use client";
 import { useState } from "react";
 import PersonCard from "../components/PersonCard";
-import { people } from "../data/mock";
+import { people, Person } from "../data/mock";
+import { Menu, Button } from "@mantine/core";
+import { IconArrowsSort, IconSortAZ, IconCalendarWeek } from '@tabler/icons-react';
+
+enum SortType {
+  Alphabetical,
+  TimePeriod
+}
 
 export default function People() {
   const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState(SortType.Alphabetical);
+
+  // Cloned to not modify imported people list
+  let peopleCards = people;
+
 
   function makePeopleCards()  {
-      return people.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
-      .map((p, idx)=> <PersonCard key={idx} name={p.name} desc={p.desc} imgURL={p.url}  />)
+      if (sortBy === SortType.Alphabetical) {
+        return peopleCards.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((p, idx)=> <PersonCard key={idx} name={p.name} desc={p.desc} imgURL={p.url}  />)
+      }
+      else {
+        return peopleCards.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
+        .sort((a, b) => a.data.yearRange.start - b.data.yearRange.start)
+        .map((p, idx)=> <PersonCard key={idx} name={p.name} desc={p.desc} imgURL={p.url}  />)
+      }
   }
 
   return (
     <div className="flex flex-col w-full justify-center items-center p-8 ">
       <div className="p-2">
         <input type="text" onChange={(e) => setSearch(e.target.value)} className="p-1 border-1 focus:outline-none focus:ring-0" />
-        <button className="bg-gray-300 p-1 m-2">Sort by</button>
+        <Menu>
+          <Menu.Target>
+            <Button p={5} m={5} leftSection={<IconArrowsSort stroke={2} />}>SORT BY</Button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item leftSection={<IconSortAZ stroke={2} />} onClick={() => setSortBy(SortType.Alphabetical)}>Alphabetical</Menu.Item>
+            <Menu.Item leftSection={<IconCalendarWeek stroke={2} />} onClick={() => setSortBy(SortType.TimePeriod)}>Time Period</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </div>
       <div className="flex w-full justify-evenly flex-wrap">
           {makePeopleCards()}
